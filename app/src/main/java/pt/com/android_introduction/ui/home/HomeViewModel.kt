61316e7a8ae96bacde596f9a3e7ffb4d7cbf86fc
liberal-henrique <luihenlibsou@gmail.com>
2023-10-20 11:51:56 +0100

    starting new windows with DrawerActivity

---
+++ app/src/main/java/pt/com/android_introduction/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package pt.com.android_introduction.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
